75f174008b060e556dd7ea8a1b8eec14708c51d6
p1r0 <p1r0@nethunters.xyz>
2025-10-19 03:14:15 -0600

    feat: updates to make it deployable in script

--- script/DappsManager.s.sol
+++ script/DappsManager.s.sol
@@ -3,17 +3,29 @@ pragma solidity ^0.8.30;
 
 import {Script, console} from "forge-std/Script.sol";
 import {DappsManager} from "../src-sc/DappsManager.sol";
+import {DappRank} from "../src-sc/DRNK.sol";
 
 contract DappsManagerScript is Script {
     DappsManager public dappsMgr;
-    uint256 listingFee = 100e16;
-    uint256 burningFee = 10; // 10%
-    uint256 daoFee = 1; // 1 %
-    uint256 bonus = 1000e18;
+    DappRank public drnkToken;
 
-    function setUp() public {}
+    uint256 listingFee = 100e16;
+    uint256 burningFee = 1000; // 10%
+    uint256 daoFee = 100; // 1 %
+    uint256 bonus = 1000e18;
+    address public owner;
+    string[] public dappsURI;
+
+    function setUp() public {
+        //dappsURI = vm.envString("dapps", '\n');
+        //owner = vm.envAddress("ACC0");
+    }
 
     function run() public {
+        //uint256 deployerPrivateKey = vm.envUint("PK0");
+
+        // startBroadcast and stopBraodcast will let us execute transactions anything between them
+        //vm.startBroadcast(deployerPrivateKey);
         vm.startBroadcast();
 
         dappsMgr = new DappsManager(
@@ -22,6 +34,11 @@ contract DappsManagerScript is Script {
             burningFee,
             bonus
         );
+        // Attach deployed token
+        drnkToken = DappRank(address(dappsMgr.drnk()));
+        console.log("-----------------------------------------------------");
+        console.log("Dapps Indexer contract address is: ", address(dappsMgr));
+        console.log("DRNK Token contract address is: ", address(address(dappsMgr.drnk())));
 
         vm.stopBroadcast();
     }
